c13b259ec4168e46a7befe9d875710ae8a839254
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-12-03 10:15:11 +0900

    First commit.

---
+++ Assets/ChangeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeNumber : MonoBehaviour
+{
+    public int number = 0 ;
+    public GameObject text;
+    public GameObject image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            number++;
+            // number += 6;
+            text.GetComponent<ChineseNumber>().setNumber(number);
+            image.GetComponent<ChangeImage>().setNumber(number);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            number--;
+            text.GetComponent<ChineseNumber>().setNumber(number);
+            image.GetComponent<ChangeImage>().setNumber(number);
+        }
+    }
+}
---
+++ Assets/ChineseNumber.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChineseNumber : MonoBehaviour
+{
+    public string[] chNums;
+    public int num;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // gameObject.GetComponent<Text>().text = chNums[num];        
+    }
+
+    public void setNumber( int n)
+    {
+        num = n;
+        gameObject.GetComponent<Text>().text = chNums[num];
+    }
+
+}
